5828ee9f0e612ca6ab0606a3601d88d96f5aa1bc
LucaCarrarini <luca.carrarini96@gmail.com>
2018-03-14 21:27:30 +0100

    aggiunto manipola dati (prima pulisce poi unisce)

--- manipola_dati.py
+++ manipola_dati.py
@@ -4,22 +4,6 @@ DATA_PATH = "./DataSet/"
 REPOSITORY = "/On_Time_On_Time_Performance_"
 CSV_PATH = "/On_Time_On_Time_Performance_" 
 year = 2015
-
-def creaDS():
-    csvs = {}
-    for month in range(1,12):
-        #for year in range(2014,2017):
-            DATA_PATH_i = DATA_PATH + str(year) + REPOSITORY + str(year) + "_" + str(month)
-            print(DATA_PATH_i)
-            CSV_PATH_i = CSV_PATH + str(year) + "_" +  str(month)
-            print(CSV_PATH_i)
-            csvs[month-1] = pd.read_csv(DATA_PATH_i + CSV_PATH_i+".csv",dtype=object)
-    result = pd.concat(csvs)
-    return result
-
-dati = creaDS()
-
-#dati.sort_values(by="DayOfMonth", inplace="True")
 useless = ["FlightDate","UniqueCarrier","Carrier","TailNum","FlightNum","OriginAirportSeqID","OriginCityMarketID","OriginCityName","OriginStateFips","OriginStateName","OriginWac","DestAirportSeqID",
 "DestCityMarketID","DestCityName","DestStateFips","DestStateName","DestWac","DepTime","DepDelay","DepDel15","DepartureDelayGroups","DepTimeBlk","TaxiOut","WheelsOff","WheelsOn","TaxiIn",
 "ArrTime","ArrDelay","ArrDel15","ArrivalDelayGroups","ArrTimeBlk","CRSElapsedTime","ActualElapsedTime","AirTime","Flights","Distance","WeatherDelay","NASDelay","SecurityDelay","LateAircraftDelay","FirstDepTime",
@@ -30,10 +14,27 @@ useless = ["FlightDate","UniqueCarrier","Carrier","TailNum","FlightNum","OriginA
 "Div4WheelsOff","Div4TailNum","Div5Airport","Div5AirportID","Div5AirportSeqID","Div5WheelsOn","Div5TotalGTime","Div5LongestGTime",
 "Div5WheelsOff","Div5TailNum"]
 
-for i in range(0,len(useless)):
-	del dati[useless[i]]
+def creaDS():
+    csvs = {}
+    for month in range(1,12):
+        DATA_PATH_i = DATA_PATH + str(year) + REPOSITORY + str(year) + "_" + str(month)
+        print(DATA_PATH_i)
+        CSV_PATH_i = CSV_PATH + str(year) + "_" +  str(month)
+        print(CSV_PATH_i)
+        csvs[month-1] = pd.read_csv(DATA_PATH_i + CSV_PATH_i+".csv",dtype=object)
+        for i in range(0,len(useless)):
+            del csvs[month-1][useless[i]]
+    result = pd.concat(csvs)
+    return result
 
-
+dati = creaDS()
 dati.pop('Unnamed: 109')
 dati.to_csv("Voli"+str(year)+".csv",sep = ';',decimal = ',')
+
+
+
+
+
+
+
 
