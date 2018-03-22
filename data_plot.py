ce693838b882a02e9a6959478111ef40ca00fe88
LucaCarrarini <luca.carrarini96@gmail.com>
2018-03-22 12:21:59 +0100

    File che plotta gennaio 2015

--- data_plot.py
+++ data_plot.py
@@ -1,8 +1,20 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from data_preparation import clean_ds, useless1, useless2
 
-PATH = "/home/luca/Scrivania/PROGETTO/perfetto_plot.csv"
-df = pd.read_csv(PATH, delimiter=";" ,encoding="utf-8-sig")
+PATH = "./DataSet/2015/On_Time_On_Time_Performance_2015_1/On_Time_On_Time_Performance_2015_1.csv"
+df = pd.read_csv(PATH, delimiter="," ,encoding="utf-8-sig")
+clean_ds(df,useless1)
+df = df[(df['OriginState'] == 'NY') & (df['DestState'] == 'CA')]
+clean_ds(df,useless2)
+df["date_time"]=df["FlightDate"].map(str)+df["DepTime"].map(str)
+df.pop("Unnamed: 109")
+df.sort_values(['FlightDate','DepTime'],axis=0,ascending=[True, True],inplace = True)
+df.to_csv("voliGennaio2015_NY_CA.csv",sep = ';',decimal = ',')
+
 df.plot(x = 'date_time', y = 'ArrDelayMinutes', kind = 'line')
-plt.savefig("gennaio2015_NY_CA.png")
+#plt.savefig("gennaio2015_NY_CA.png")
 plt.show()
+
+
+#dati per plot
