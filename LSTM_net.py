60d42bf44e63c5c9db0d5a75f824da14f9a504c8
ariannagavioli <gavioli.arianna@gmail.com>
2018-03-27 11:47:22 +0200

    File per la rete (aggiunti batch)

--- LSTM_net.py
+++ LSTM_net.py
@@ -2,27 +2,35 @@ import numpy
 #import matplotlib.pyplot as plt
 import pandas as pd
 import math
-from keras.models import Sequential, load_model
+from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
+MB_SIZE = 6793
+STEP_PER_EPOCH = 6
 LOOK_BACK = 70
 DENSE_BLOCKS = 1
 LSTM_BLOCKS = 4
-EPOCHS = 1
+EPOCHS = 15
 PROVA = 1
-PATH_TRAIN = "./DataSet/train_set.csv"
-PATH_TEST = "./DataSet/test_set.csv"
+PATH_TRAIN = "./DataSet/train_subset.csv"
+PATH_TEST = "./DataSet/test_subset.csv"
 
 train = pd.read_csv(PATH_TRAIN,delimiter=';')
 test = pd.read_csv(PATH_TEST,delimiter=';')
 
 train.pop('FlightDate')
 train.pop('Unnamed: 109')
+train.pop('Unnamed: 0')
+train.pop('Unnamed: 1')
+train.pop('Unnamed: 2')
 test.pop('FlightDate')
 test.pop('Unnamed: 109')
+test.pop('Unnamed: 0')
+test.pop('Unnamed: 1')
+test.pop('Unnamed: 2')
 train.pop('CancellationCode')
 test.pop('CancellationCode')
 
@@ -41,6 +49,7 @@ test = scaler.fit_transform(test)
         print('\n')
         if(dataset[0,i]=='ArrDelayMinutes'): return i
     return -1'''
+    
 
 #Creo un nuovo dataset con timestamp consecutivi su una stessa riga
 #Di fatto avrò i voli precedenti al volo i in voliPrecedenti[i], il ritardo da
@@ -61,6 +70,16 @@ def create_dataset(dataset, look_back=LOOK_BACK):
         voliPrecedenti.append(x)
         ritardiEffettivi.append(dataset[i+look_back,15])
     return numpy.array(voliPrecedenti), numpy.array(ritardiEffettivi)
+
+#Definisco una funzione generatore di batch per il fit del modello:
+def generator(batch_size):
+    counter = 0
+    while True:
+        inputs = voliPrecedenti[counter:counter+batch_size]
+        targets = ritardiEffettivi[counter:counter+batch_size]
+        counter += 1
+        yield (inputs,targets)
+
 
 voliPrecedenti, ritardiEffettivi = create_dataset(train)
 testVoliPrecedenti, testRitardiEffettivi = create_dataset(test)
@@ -83,27 +102,28 @@ testVoliPrecedenti = numpy.reshape(testVoliPrecedenti, (testVoliPrecedenti.shape
 
 #Creo la rete e la alleno
 model = Sequential()
-#EVENTUALMENTE INIZIALIZZARE I PESI
 #????? Proviamo a scambiare
-model.add(LSTM(LSTM_BLOCKS, input_shape = (LOOK_BACK,voliPrecedenti.shape[2])))
-model.add(Dense(DENSE_BLOCKS))
+model.add(LSTM(LSTM_BLOCKS, input_shape = (LOOK_BACK,voliPrecedenti.shape[2]),kernel_initializer='random_uniform',bias_initializer='zeros'))
+model.add(Dense(DENSE_BLOCKS,kernel_initializer='random_uniform',bias_initializer='zeros'))
 #SGD = Stochastic Gradient Descent
 model.compile(optimizer = 'SGD', loss = 'mean_squared_error')
+minibatch_size = MB_SIZE
 #Batch size -> ogni quanto aggiorniamo il gradient descent, batch size di default 32
-model.fit(voliPrecedenti, ritardiEffettivi, epochs = EPOCHS, batch_size=1, verbose=1,
-          validation_split=0.2)
-model.save("LSTN_prova"+str(PROVA)+".h5")
+#EVENTUALMENTE METTERE VALIDATION SET
+model.fit_generator(generator(minibatch_size), steps_per_epoch = STEP_PER_EPOCH, epochs = EPOCHS, verbose=1,
+                    use_multiprocessing = True)
+model.save("LSTM_prova"+str(PROVA)+".h5")
 
 #Prediciamo i ritardi
-ritardiPredetti = model.predict(ritardiEffettivi)
-testPredict = model.predict(testRitardiEffettivi)
+ritardiPredetti = model.predict(voliPrecedenti,verbose=1)
+testPredict = model.predict(testVoliPrecedenti,verbose=1)
 #Riportiamo i dati normalizzati tra (0,1) nelle unità originali
-ritardiPredetti = scaler.inverse_transform(ritardiPredetti)
-ritardiEffettivi = scaler.inverse_transform([ritardiEffettivi])
-testPredict = scaler.inverse_transform(testPredict)
-testRitardiEffettivi = scaler.inverse_transform([testRitardiEffettivi])
+#ritardiPredetti = scaler.inverse_transform(ritardiPredetti)
+#ritardiEffettivi = scaler.inverse_transform([ritardiEffettivi])
+#testPredict = scaler.inverse_transform(testPredict)
+#testRitardiEffettivi = scaler.inverse_transform([testRitardiEffettivi])
 #Calcolo l'errore con lo scarto quadratico medio
-trainError = math.sqrt(mean_squared_error(ritardiEffettivi[0],ritardiPredetti.iloc[:,0])) 
+trainError = math.sqrt(mean_squared_error(ritardiEffettivi,ritardiPredetti)) 
 print("Errore di train: %.2f RMSE" % (trainError))
-testError = math.sqrt(mean_squared_error(testRitardiEffettivi[0],testPredict.iloc[:,0])) 
+testError = math.sqrt(mean_squared_error(testRitardiEffettivi,testPredict)) 
 print("Errore di test: %.2f RMSE" % (testError))
