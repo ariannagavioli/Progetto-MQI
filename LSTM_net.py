a2d6543ce39f034c34424281320e2b16a95c5b64
ariannagavioli <gavioli.arianna@gmail.com>
2018-04-05 12:08:03 +0200

    File per rete LSTM  vecchio approccio (aggiunta colonna features volo attuale)

--- LSTM_net.py
+++ LSTM_net.py
@@ -7,16 +7,21 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
-#macros
-MB_SIZE = 6793
-STEP_PER_EPOCH = 6
-LOOK_BACK = 70
-LSTM_NEURONS = 50
-DENSE_NEURONS = 1
-EPOCHS = 15
-PROVA = 1
+from data_preparation import obtain_index
+
+#MACROS
+MB_SIZE = 6793          #dimensione del minibatch
+STEP_PER_EPOCH = 6      #numero dei minibatch nel dataset
+LOOK_BACK = 70          #numero dei voli nella finestra temporale precedente
+LSTM_NEURONS = 50       #numero neuroni LSTM
+DENSE_NEURONS = 1       #numero neuroni Dense
+EPOCHS = 15             #numero di epoche per l'allenamento
+PROVA = 1               #numero di prova per salvare l'allenamento
+
 PATH_TRAIN = "./DataSet/train_subset.csv"
 PATH_TEST = "./DataSet/test_subset.csv"
+
+
 #Importo train e test
 train = pd.read_csv(PATH_TRAIN,delimiter=';')
 test = pd.read_csv(PATH_TEST,delimiter=';')
@@ -26,38 +31,51 @@ from data_preparation import clean_ds
 useless = ['FlightDate', 'Unnamed: 109', 'Unnamed: 0', 'Unnamed: 1', 'Unnamed: 2', 'CancellationCode']
 clean_ds(train, useless)
 clean_ds(test, useless)
-'''
-train.pop('FlightDate')
-train.pop('Unnamed: 109')
-train.pop('Unnamed: 0')
-train.pop('Unnamed: 1')
-train.pop('Unnamed: 2')
-test.pop('FlightDate')
-test.pop('Unnamed: 109')
-test.pop('Unnamed: 0')
-test.pop('Unnamed: 1')
-test.pop('Unnamed: 2')
-train.pop('CancellationCode')
-test.pop('CancellationCode')
-'''
-#il dataset viene castato a float
+
+#Il dataset viene castato a float
 train.astype(dtype = 'float64')
 test.astype(dtype = 'float64')
 
 #Creo un nuovo dataset con timestamp consecutivi su una stessa riga
 #Di fatto avrò i voli precedenti al volo i in voliPrecedenti[i], il ritardo da
 #predire del volo i in ritardiEffettivi[i]
-    
 def create_dataset(dataset, look_back=LOOK_BACK):
     #Inizializzo voliPrecedenti a zero con dimensioni (numero voli da predire, time step precedenti, numero di feature)
     #Inizializzo ritardiEffettivi a zero con dimensioni (numero voli da predire, 1)
-    voliPrecedenti = numpy.zeros((dataset.shape[0]-look_back,look_back,dataset.shape[1]),dtype=numpy.float)
+    voliPrecedenti = numpy.zeros((dataset.shape[0]-look_back,look_back+1,dataset.shape[1]),dtype=numpy.float)
     ritardiEffettivi = numpy.zeros((dataset.shape[0]-look_back,1),dtype=numpy.float)
     for i in range(dataset.shape[0]-look_back):
-        x = dataset[i:(i+look_back)]
+        if(i%2): print(i)
+        x = dataset[i:(i+look_back+1)]
+        #x[i+look_back] contiene le feature del volo di cui vogliamo predire il ritardo
+        #impostiamo i valori DepDelayMinutes, ArrDelayMinutes, ... alla media dei ritardi della 
+        #finestra precedente (i:i+look_back), in modo da non influenzare la predizione del ritardo
+        dep_delay = obtain_index(dataset,"DepDelayMinutes")
+        arr_delay = obtain_index(dataset,"ArrDelayMinutes")
+        carrier_delay = obtain_index(dataset,"CarrierDelay")
+        weather_delay = obtain_index(dataset,"WeatherDelay")
+        nas_delay = obtain_index(dataset,"NASDelay")
+        security_delay = obtain_index(dataset,"SecurityDelay")
+        lateairc_delay = obtain_index(dataset,"LateAircraftDelay")
+        arr_time = obtain_index(dataset,"ArrTime")
+        crs_arr_time = obtain_index(dataset,"CRSArrTime")
+        x.iloc[look_back,dep_delay] = average(x.iloc[0:(look_back)],dep_delay)
+        x.iloc[look_back,arr_delay] = average(x.iloc[0:(look_back)],arr_delay)
+        x.iloc[look_back,carrier_delay] = average(x.iloc[0:(look_back)],carrier_delay)
+        x.iloc[look_back,weather_delay] = average(x.iloc[0:(look_back)],weather_delay)
+        x.iloc[look_back,nas_delay] = average(x.iloc[0:(look_back)],nas_delay)
+        x.iloc[look_back,security_delay] = average(x.iloc[0:(look_back)],security_delay)
+        x.iloc[look_back,lateairc_delay] = average(x.iloc[0:(look_back)],lateairc_delay)
+        #imposto l'orario di arrivo all'orario previsto di arrivo + minuti di ritardo previsti
+        #in modo naif (media) poiché non abbiamo informazioni sull'effettivo ritardo
+        x.iloc[look_back,arr_time] = x.iloc[look_back,crs_arr_time] + x.iloc[look_back,arr_delay]
         voliPrecedenti[i] = x
-        ritardiEffettivi[i] = dataset.iloc[i+look_back,12]
+        ritardiEffettivi[i] = dataset.iloc[i+look_back,arr_delay]
     return voliPrecedenti,ritardiEffettivi
+
+#Definisco una funzione che ritorna la media di una colonna di un dataframe
+def average(dataset,index):
+    return numpy.mean(dataset.iloc[index])
 
 #Definisco una funzione generatore di batch per il fit del modello:
 def generator(batch_size):
@@ -70,6 +88,12 @@ def generator(batch_size):
 
 voliPrecedenti, ritardiEffettivi = create_dataset(train)
 testVoliPrecedenti, testRitardiEffettivi = create_dataset(test)
+
+voliPrecedenti.to_csv("voliPrecedenti.csv",sep=';',decimal=',')
+ritardiEffettivi.to_csv("ritardiEffettivi.csv",sep=';',decimal=',')
+testVoliPrecedenti.to_csv("testVoliPrecedenti.csv",sep=';',decimal=',')
+testRitardiEffettivi.to_csv("testRitardiEffettivi.csv",sep=';',decimal=',')
+
 
 #Normalizzo i valori nei set
 scaler1 = MinMaxScaler(feature_range=(0,1))
@@ -90,9 +114,11 @@ testVoliPrecedenti = numpy.reshape(testVoliPrecedenti, (testVoliPrecedenti.shape
 
 #Creo la rete e la alleno
 model = Sequential()
-#????? Proviamo a scambiare
+
 #COME PRIMO PARAMETRO I LAYER VOGLIONO IL NUMERO DI NEURONI
-model.add(LSTM(LSTM_NEURONS, input_shape = (LOOK_BACK,voliPrecedenti.shape[2]),kernel_initializer='random_uniform',bias_initializer='zeros'))
+model.add(LSTM(LSTM_NEURONS, input_shape = (LOOK_BACK,voliPrecedenti.shape[2]),
+                                            kernel_initializer='random_uniform',bias_initializer='zeros',
+                                            activation = 'sigmoid'))
 model.add(Dense(DENSE_NEURONS,kernel_initializer='random_uniform',bias_initializer='zeros'))
 #SGD = Stochastic Gradient Descent
 model.compile(optimizer = 'SGD', loss = 'mean_squared_error')
